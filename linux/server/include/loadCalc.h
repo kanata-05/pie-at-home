950bd0b6c9a7e6e7e7a2fc44e5c5624230d315e4
tails <169904231+kanata-05@users.noreply.github.com>
2025-04-15 00:21:42 +0900

    dynamic task calc

--- linux/server/include/loadCalc.h
+++ linux/server/include/loadCalc.h
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct {
+    int start;
+    int end;
+} range;
+
+range getRange(int start, float score) {
+    int max_factor = 10000; // scale number range size (0.1 = 1000)
+    int end = start + (score * max_factor);
+
+    r.start = start;
+    r.end = end;
+    return r;
+}
